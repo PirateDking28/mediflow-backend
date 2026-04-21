802dd43b3c3ea89830df97c12207ad66b1139593
ArmandoM <meza.lopez.armando@gmail.com>
2026-04-21 14:45:44 -0700

    fix: configurar cors para vercel

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: 'https://mediflow-frontend.vercel.app',
     credentials: true
 }));
 app.use(express.json());
